0a5cc116dadb65fd6bb9d88fa40826c30c99bcda
DevCode0456 <muhmmmadusman0456@gmail.com>
2024-12-04 09:53:50 +0000

    variables and conversion

--- basic/variables.js
+++ basic/variables.js
@@ -1,10 +1,40 @@
-const accountId =  2324 ;
+// ************************** Variables *************************
 
-let accountName = "DevCode"
+// const accountId =  2324 ;
 
-var useremail = "devemail@gmail.com"
+// let accountName = "DevCode"
 
-console.log(accountId);
-console.log(accountName);
-console.log(useremail);
+// var useremail = "devemail@gmail.com"
+// console.log(accountName);
+// console.log("Usman Anwaar");
+// let password =  undefined
+// null
+
+// console.table([accountId,accountName,useremail,password
+//    , null]);
+// console.log(typeof accountId);
+// console.log( TypeError);
+// ************************** Conversion of datatypes *************************
+
+// let score = "343gg"
+// score = null
+// console.log(score);
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let convertIntoNumber = Number(score);
+// console.log(typeof convertIntoNumber);
+// console.log(convertIntoNumber);
+
+// ************************** Opertions *************************
+let str1 = " Muhmmad"
+let str2 =" Usman"
+let str3 = " Anwaar"
+let str4 = str1 + str2 +str3
+console.log(str4);
+
+
+
+
 
